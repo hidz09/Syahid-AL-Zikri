45d37690b7ee422f0bc1587eb7eb6c4cbe03a8f6
hidz09 <msyahidalzikri314@gmail.com>
2025-02-05 14:47:22 +0700

    Add files via upload

---
+++ latihan3.java
@@ -0,0 +1,13 @@
+
+public class latihan3 {
+    // Menggunakan operator aritmatika
+    public static void main(String[] args) {
+        int A = 70;
+        int B = 80;
+        int C = 90;
+        boolean a = A > B && B < C || C < B;
+        boolean b = A < B || B < C && C < B;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
